7184249f15302a70c37fb7115f867df22a614879
Naendo <56106169+Naendo@users.noreply.github.com>
2020-10-21 10:13:59 +0200

    Init Samples

---
+++ TwitchWrapper.Samples/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TwitchWrapper.Core;
+
+namespace TwitchWrapper.Samples
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+            => await new TwitchController().InitializeTwitchClient();
+    }
+
+
+    public class TwitchController
+    {
+        private readonly TwitchBot _twitchBot;
+
+        public TwitchController()
+        {
+            _twitchBot = new TwitchBot();
+        }
+
+        public async Task InitializeTwitchClient()
+        {
+            await _twitchBot.LoginAsync("nick", "oauth:token");
+
+            await _twitchBot.JoinAsync("yourChannel");
+
+
+            var commander = new TwitchCommander(_twitchBot);
+
+            await commander.InitalizeCommanderAsync(
+                serviceCollection: BuildServiceCollection(),
+                assembly: Assembly.GetEntryAssembly()
+            );
+        }
+
+        private static IServiceCollection BuildServiceCollection()
+            => new ServiceCollection();
+    }
+}
